7566bbb0fac7d350ffbd714fd5816468715d925c
Cristi Dima <thecristidima@gmail.com>
2025-03-09 10:33:32 +0200

    Reverse arrow keys for changing width/height

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,10 +8,10 @@ map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = tru
 
 
 -- Resize window using <ctrl> arrow keys
-map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+map("n", "<C-Down>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Up>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Right>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Left>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
 -- Move lines (wip)
 map("n", "<A-Down>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
