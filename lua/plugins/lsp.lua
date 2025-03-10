67c9ec961961e3a24a44de5e02fab32fe34d1ca7
Cristi <cristian.dima@uipath.com>
2025-03-10 14:03:33 +0200

    Add keymaps to show docs, rename, go to definition/implementation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,29 @@
+local on_attach = function(_, buff)
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = "LSP: " .. desc
+        end
+        vim.keymap.set("n", keys, func, { buffer = buff, desc = desc })
+    end
+
+    nmap("<leader>cr", vim.lsp.buf.rename, "Rename Symbol")
+    nmap("<leader>ca", function()
+        vim.lsp.buf.code_action { context = { only = { "quickfix", "refactor", "source" } } }
+    end, "Code Action")
+    nmap("<leader>cf", vim.lsp.buf.format, "Format Code")
+    nmap("<leader>cgd", vim.lsp.buf.definition, "Go to Definition")
+    nmap("<leader>cgi", vim.lsp.buf.implementation, "Go to Implementation")
+    nmap("<leader>cgr", vim.lsp.buf.references, "Go to References")
+    nmap("<leader>cD", vim.lsp.buf.document_symbol, "Show symbol documentation")
+end
+
 return {
-    -- TODO We need nvim-lspconfig and mason
-    -- We might need mason-lspconfig in the future, but for now we just want LSP in C# and maybe json and yaml
     {
         "neovim/nvim-lspconfig",
         dependencies = {
             "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim"
+            "williamboman/mason-lspconfig.nvim",
+            { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true }
         },
         config = function()
             require("mason").setup()
@@ -14,14 +32,12 @@ return {
             })
 
             require("mason-lspconfig").setup_handlers {
-                function (server_name)
-                    require("lspconfig")[server_name].setup {}
+                function(server_name)
+                    require("lspconfig")[server_name].setup({
+                        on_attach = on_attach
+                    })
                 end,
 
-                -- In case you want a special config for a certain server
-                -- ["csharp_ls"] = function()
-                    -- require("csharp_ls").setup {}
-                -- end
                 ["omnisharp"] = function()
                     require("lspconfig").omnisharp.setup({
                         cmd = { "dotnet", vim.fn.stdpath "data" .. "/mason/packages/omnisharp/libexec/OmniSharp.dll" },
@@ -30,7 +46,8 @@ return {
                         enable_roslyn_analyzers = true,
                         root_dir = function()
                             return vim.loop.cwd() -- current working directory
-                        end
+                        end,
+                        on_attach = on_attach
                     })
                 end
             }
