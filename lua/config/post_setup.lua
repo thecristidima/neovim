599e72c8d8d189833d48c6df42bf8f65cae5da4d
Cristi Dima <thecristidima@gmail.com>
2025-10-19 22:00:23 +0300

    Replace omnisharp with roslyn.nvim
    Tested on smaller projects and it seems to be working fine,
    but there are a couple of things missing, such as support
    for displaying errors inline, at the moment it just shows an E
    in the gutter (but this might be fixed with a plugin?).
    
    Also tested this on macOS and I need to do some keybinding fixes
    for it, because I might have focused only on Windows, but it's not
    that important at the moment.
    
    Also, some spellchecker thingy might be useful.

--- lua/config/post_setup.lua
+++ lua/config/post_setup.lua
@@ -1,5 +1,26 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-    callback = function()
-        vim.cmd("lua vim.lsp.buf.format()")
-    end
+	callback = function()
+		vim.cmd("lua vim.lsp.buf.format()")
+	end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(ev)
+		local nmap = function(keys, func, desc)
+			if desc then
+				desc = "LSP: " .. desc
+			end
+			vim.keymap.set("n", keys, func, { buffer = buff, desc = desc })
+		end
+
+		nmap("<leader>cr", vim.lsp.buf.rename, "Rename Symbol")
+		nmap("<leader>ca", function()
+			vim.lsp.buf.code_action({ context = { only = { "quickfix", "refactor", "source" } } })
+		end, "Code Action")
+		nmap("<leader>cf", vim.lsp.buf.format, "Format Code")
+		nmap("<leader>cgd", vim.lsp.buf.definition, "Go to Definition")
+		nmap("<leader>cgi", vim.lsp.buf.implementation, "Go to Implementation")
+		nmap("<leader>cgr", vim.lsp.buf.references, "Go to References")
+		nmap("<leader>cD", vim.lsp.buf.document_symbol, "Show symbol documentation")
+	end,
 })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,56 +1,39 @@
 local on_attach = function(_, buff)
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = "LSP: " .. desc
-        end
-        vim.keymap.set("n", keys, func, { buffer = buff, desc = desc })
-    end
+	local nmap = function(keys, func, desc)
+		if desc then
+			desc = "LSP: " .. desc
+		end
+		vim.keymap.set("n", keys, func, { buffer = buff, desc = desc })
+	end
 
-    nmap("<leader>cr", vim.lsp.buf.rename, "Rename Symbol")
-    nmap("<leader>ca", function()
-        vim.lsp.buf.code_action { context = { only = { "quickfix", "refactor", "source" } } }
-    end, "Code Action")
-    nmap("<leader>cf", vim.lsp.buf.format, "Format Code")
-    nmap("<leader>cgd", vim.lsp.buf.definition, "Go to Definition")
-    nmap("<leader>cgi", vim.lsp.buf.implementation, "Go to Implementation")
-    nmap("<leader>cgr", vim.lsp.buf.references, "Go to References")
-    nmap("<leader>cD", vim.lsp.buf.document_symbol, "Show symbol documentation")
+	nmap("<leader>cr", vim.lsp.buf.rename, "Rename Symbol")
+	nmap("<leader>ca", function()
+		vim.lsp.buf.code_action({ context = { only = { "quickfix", "refactor", "source" } } })
+	end, "Code Action")
+	nmap("<leader>cf", vim.lsp.buf.format, "Format Code")
+	nmap("<leader>cgd", vim.lsp.buf.definition, "Go to Definition")
+	nmap("<leader>cgi", vim.lsp.buf.implementation, "Go to Implementation")
+	nmap("<leader>cgr", vim.lsp.buf.references, "Go to References")
+	nmap("<leader>cD", vim.lsp.buf.document_symbol, "Show symbol documentation")
 end
 
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true }
-        },
-        config = function()
-            require("mason").setup()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "jsonls", "lemminx", "yamlls", "omnisharp" }
-            })
-
-            require("mason-lspconfig").setup_handlers {
-                function(server_name)
-                    require("lspconfig")[server_name].setup({
-                        on_attach = on_attach
-                    })
-                end,
-
-                ["omnisharp"] = function()
-                    require("lspconfig").omnisharp.setup({
-                        cmd = { "dotnet", vim.fn.stdpath "data" .. "/mason/packages/omnisharp/libexec/OmniSharp.dll" },
-                        enable_import_completion = true,
-                        organize_imports_on_format = true,
-                        enable_roslyn_analyzers = true,
-                        root_dir = function()
-                            return vim.loop.cwd() -- current working directory
-                        end,
-                        on_attach = on_attach
-                    })
-                end
-            }
-        end
-    }
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("mason").setup({
+				registries = {
+					"github:mason-org/mason-registry",
+					"github:Crashdummyy/mason-registry",
+				},
+			})
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "jsonls", "lemminx", "yamlls", "roslyn" },
+			})
+		end,
+	},
 }
