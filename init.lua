2509892cb765c1fe77c05c998b28aec4edd8b4aa
Cristi Dima <thecristidima@gmail.com>
2026-01-01 12:07:46 +0200

    Add global vars for OS type

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+-- Define some global variables to be available in all other scripts
+local uname = vim.loop.os_uname()
+_G.IS_MAC = uname.sysname == "Darwin"
+_G.IS_LINUX = uname.sysname == "Linux"
+_G.IS_WINDOWS = uname.sysname == "Windows_NT"
+
 require("config.editor")
 require("config.lazy")
 require("config.keymaps")
