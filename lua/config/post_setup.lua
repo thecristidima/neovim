b5a3d8bdc3167c62344d919381b6f130a69e5d39
Cristi Dima <thecristidima@gmail.com>
2026-01-01 14:25:01 +0200

    Change tab size

--- lua/config/post_setup.lua
+++ lua/config/post_setup.lua
@@ -1,41 +1,41 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function()
-		vim.cmd("lua vim.lsp.buf.format()")
-	end,
+    callback = function()
+        vim.cmd("lua vim.lsp.buf.format()")
+    end,
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(ev)
-		local nmap = function(keys, func, desc)
-			if desc then
-				desc = "LSP: " .. desc
-			end
-			vim.keymap.set("n", keys, func, { buffer = buff, desc = desc })
-		end
+    callback = function(ev)
+        local nmap = function(keys, func, desc)
+            if desc then
+                desc = "LSP: " .. desc
+            end
+            vim.keymap.set("n", keys, func, { buffer = buff, desc = desc })
+        end
 
-		nmap("<leader>cr", vim.lsp.buf.rename, "Rename Symbol")
-		nmap("<leader>ca", function()
-			vim.lsp.buf.code_action({ context = { only = { "quickfix", "refactor", "source" } } })
-		end, "Code Action")
-		nmap("<leader>cf", vim.lsp.buf.format, "Format Code")
-		nmap("<leader>cgd", vim.lsp.buf.definition, "Go to Definition")
-		nmap("<leader>cgi", vim.lsp.buf.implementation, "Go to Implementation")
-		nmap("<leader>cgr", vim.lsp.buf.references, "Go to References")
-		nmap("<leader>cD", vim.lsp.buf.document_symbol, "Show symbol documentation")
+        nmap("<leader>cr", vim.lsp.buf.rename, "Rename Symbol")
+        nmap("<leader>ca", function()
+            vim.lsp.buf.code_action({ context = { only = { "quickfix", "refactor", "source" } } })
+        end, "Code Action")
+        nmap("<leader>cf", vim.lsp.buf.format, "Format Code")
+        nmap("<leader>cgd", vim.lsp.buf.definition, "Go to Definition")
+        nmap("<leader>cgi", vim.lsp.buf.implementation, "Go to Implementation")
+        nmap("<leader>cgr", vim.lsp.buf.references, "Go to References")
+        nmap("<leader>cD", vim.lsp.buf.document_symbol, "Show symbol documentation")
 
-		-- This enables inline diagnostic text and sets its colour to whatever the gutter icon uses
-		vim.diagnostic.config({ virtual_text = true })
-		vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { link = "DiagnosticSignError" })
-		vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { link = "DiagnosticSignWarn" })
-		vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { link = "DiagnosticSignInfo" })
-		vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { link = "DiagnosticSignHint" })
-	end,
+        -- This enables inline diagnostic text and sets its colour to whatever the gutter icon uses
+        vim.diagnostic.config({ virtual_text = true })
+        vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { link = "DiagnosticSignError" })
+        vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { link = "DiagnosticSignWarn" })
+        vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { link = "DiagnosticSignInfo" })
+        vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { link = "DiagnosticSignHint" })
+    end,
 })
 
 -- Command to toggle inline diagnostics
 vim.api.nvim_create_user_command("ToggleInlineDiagnostic", function()
-	local new_virtual_text = not vim.diagnostic.config().virtual_text
-	vim.diagnostic.config({ virtual_text = new_virtual_text })
+    local new_virtual_text = not vim.diagnostic.config().virtual_text
+    vim.diagnostic.config({ virtual_text = new_virtual_text })
 end, {})
 
 -- Command to toggle spellchecker
