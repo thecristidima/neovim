a52d9ae422b08ba08c598ed6beca93a14e82185c
Cristi <cristian.dima@uipath.com>
2026-01-15 14:54:07 +0200

    Fix deprecated method warning

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -15,7 +15,7 @@ return {
                 lualine_y = {
                     {
                         function()
-                            local lsps = vim.lsp.get_active_clients({ bufnr = vim.fn.bufnr() })
+                            local lsps = vim.lsp.get_clients({ bufnr = vim.fn.bufnr() })
                             local icon = require("nvim-web-devicons").get_icon_by_filetype(
                                 vim.api.nvim_buf_get_option(0, "filetype")
                             )
