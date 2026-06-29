73e00deac251f071602bb6c1519e82cba07da61b
Cristi <cristian.dima@uipath.com>
2026-06-29 14:59:53 +0300

    Remove unused flash keymaps + small style change for lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,11 @@ return {
     event = "VeryLazy",
     config = function()
         require("lualine").setup {
+            options = {
+                theme = "gruvbox-material",
+                component_separators = { left = "|", right = "|" },
+                section_separators = { left = "", right = "" },
+            },
             sections = {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diff', 'diagnostics' },
